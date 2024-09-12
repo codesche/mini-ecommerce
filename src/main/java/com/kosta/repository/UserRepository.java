2037257f6c9c784d726595cb868126c4af55f956
codesche <codesche@gmail.com>
2024-09-12 11:46:06 +0900

    feat: UserRepository 추가

---
+++ src/main/java/com/kosta/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.kosta.repository;
+
+import com.kosta.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
