faaadce7d0095eb5d144e129764c75055ff0d4ac
codesche <codesche@gmail.com>
2024-09-12 10:28:41 +0900

    feat: 이미지 파일 업로드 위한 Repository 생성

---
+++ src/main/java/com/kosta/repository/ImageFileRepository.java
@@ -0,0 +1,10 @@
+package com.kosta.repository;
+
+import com.kosta.entity.ImageFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageFileRepository extends JpaRepository<ImageFile, Long> {
+
+}
