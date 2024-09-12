86ff30d8199d57a293b8a2671b65b1902f26de18
codesche <codesche@gmail.com>
2024-09-12 11:46:50 +0900

    feat: 이메일 조회 관련 로직 구현

---
+++ src/main/java/com/kosta/service/UserDetailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.kosta.service;
+
+import com.kosta.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("[loadUserByUsername] 사용자 이메일 조회. username : {}", email);
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("없는 이메일"));
+
+    }
+}
