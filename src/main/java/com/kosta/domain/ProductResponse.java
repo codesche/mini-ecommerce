2b6045611a59dd0ee10d8e090bec50fe1d0cd6b9
codesche <codesche@gmail.com>
2024-09-12 09:32:08 +0900

    feat: ResponseDTO 생성

---
+++ src/main/java/com/kosta/domain/ProductResponse.java
@@ -0,0 +1,26 @@
+package com.kosta.domain;
+
+import com.kosta.entity.Product;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProductResponse {
+
+    private Long id;
+    private String name;
+    private String password;
+    private int price;
+    private ProductResponse seller;
+
+    public static ProductResponse toDTO(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .password(product.getPassword())
+                .build();
+    }
+
+}
